7b793924c3756b87d9ee41f93f5c99eab0899fda
Alex Tsvetanov <alex.tsvetanov@chaos.com>
2025-04-15 13:50:38 +0300

    Fix compilation

--- tests/test_pubsub.cpp
+++ tests/test_pubsub.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch_all.hpp>
-#include "pubsub.hpp"
+#include "pubsub.h"
 
 using namespace pubsub;
 
@@ -11,11 +11,11 @@ struct TestEvents {
 
 class TestSubscriber : public Subscriber {
 public:
-    int count = 0;
-    int last_value = -1;
+    int ping_count = 0;
+    int data_sum = 0;
 
-    void handlePing() { count++; }
-    void handleData(int val) { last_value = val; }
+    void handlePing() { ping_count++; }
+    void handleData(int val) { data_sum += val; }
 
     void subscribe_to(Publisher& p) override {
         store_token(p.subscribe<TestEvents::Ping>(this, &TestSubscriber::handlePing));
@@ -39,8 +39,8 @@ TEST_CASE("Basic pub-sub works", "[pubsub]") {
     pub.emit<TestEvents::Ping>();
     pub.emit<TestEvents::Data>(42);
 
-    REQUIRE(sub.count == 2);
-    REQUIRE(sub.last_value == 42);
+    REQUIRE(sub.ping_count == 2);
+    REQUIRE(sub.data_sum == 42);
 }
 
 TEST_CASE("Auto unsubscribe on destruction", "[pubsub]") {
@@ -49,11 +49,49 @@ TEST_CASE("Auto unsubscribe on destruction", "[pubsub]") {
         TestSubscriber sub;
         sub.subscribe_to(pub);
         pub.emit<TestEvents::Ping>();
-        REQUIRE(sub.count == 1);
+        REQUIRE(sub.ping_count == 1);
     }
 
-    // Should not crash or double-invoke
     REQUIRE_NOTHROW(pub.emit<TestEvents::Ping>());
+}
+
+TEST_CASE("Subscribing the same object twice does not double subscribe") {
+    Publisher pub;
+    TestSubscriber sub;
+
+    sub.subscribe_to(pub);
+    sub.subscribe_to(pub);
+
+    pub.emit<TestEvents::Ping>();
+    pub.emit<TestEvents::Data>(10);
+
+    REQUIRE(sub.ping_count == 1);
+    REQUIRE(sub.data_sum == 10);
+}
+
+TEST_CASE("Unsubscribing an object that was never subscribed is safe") {
+    Publisher pub;
+    TestSubscriber sub;
+
+    pub.unsubscribe<TestEvents::Ping>(&sub);
+    pub.emit<TestEvents::Ping>();
+    REQUIRE(sub.ping_count == 0);
+}
+
+TEST_CASE("Emitting with no subscribers does nothing and is safe") {
+    Publisher pub;
+    REQUIRE_NOTHROW(pub.emit<TestEvents::Ping>());
+    REQUIRE_NOTHROW(pub.emit<TestEvents::Data>(42));
+}
+
+TEST_CASE("Async event delivery still invokes subscribers") {
+    Publisher pub;
+    TestSubscriber sub;
+
+    sub.subscribe_to(pub);
+    pub.emit_async<TestEvents::Data>(99);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    REQUIRE(sub.data_sum == 99);
 }
 
 TEST_CASE("Subscriber destruction triggers unsubscribe and prevents memory leaks") {
@@ -67,24 +105,24 @@ TEST_CASE("Subscriber destruction triggers unsubscribe and prevents memory leaks
             TempSubscriber(int& count) : call_count_ref(count) {}
 
             void subscribe_to(Publisher& pub) override {
-                store_token(pub.subscribe<TestEvents::A>(this, &TempSubscriber::handle));
+                store_token(pub.subscribe<TestEvents::Ping>(this, &TempSubscriber::handle));
                 Subscriber::subscribe_to(pub);
             }
 
             void unsubscribe_from(Publisher& pub) override {
-                pub.unsubscribe<TestEvents::A>(this);
+                pub.unsubscribe<TestEvents::Ping>(this);
             }
 
             void handle() { call_count_ref++; }
         } s(call_count);
 
         s.subscribe_to(pub);
-        pub.emit<TestEvents::A>();
+        pub.emit<TestEvents::Ping>();
         REQUIRE(call_count == 1);
     }
 
-    pub.emit<TestEvents::A>();
-    REQUIRE(call_count == 1); // Should not increment after subscriber is destroyed
+    pub.emit<TestEvents::Ping>();
+    REQUIRE(call_count == 1);
 }
 
 TEST_CASE("Multiple subscribers are cleaned up without memory leaks") {
@@ -98,12 +136,12 @@ TEST_CASE("Multiple subscribers are cleaned up without memory leaks") {
             MultiSub(int& total) : ref(total) {}
 
             void subscribe_to(Publisher& pub) override {
-                store_token(pub.subscribe<TestEvents::A>(this, &MultiSub::on_event));
+                store_token(pub.subscribe<TestEvents::Ping>(this, &MultiSub::on_event));
                 Subscriber::subscribe_to(pub);
             }
 
             void unsubscribe_from(Publisher& pub) override {
-                pub.unsubscribe<TestEvents::A>(this);
+                pub.unsubscribe<TestEvents::Ping>(this);
             }
 
             void on_event() { ref++; }
@@ -112,51 +150,10 @@ TEST_CASE("Multiple subscribers are cleaned up without memory leaks") {
         sub1.subscribe_to(pub);
         sub2.subscribe_to(pub);
 
-        pub.emit<TestEvents::A>();
+        pub.emit<TestEvents::Ping>();
         REQUIRE(total_calls == 2);
     }
 
-    pub.emit<TestEvents::A>();
-    REQUIRE(total_calls == 2); // No additional calls after destruction
-}
-
-TEST_CASE("Subscribing the same object twice does not double subscribe") {
-    Publisher pub;
-    TestSubscriber sub;
-
-    sub.subscribe_to(pub); // first subscribe
-    sub.subscribe_to(pub); // second call should not double-subscribe
-
-    pub.emit<TestEvents::A>();
-    pub.emit<TestEvents::B>(10);
-
-    REQUIRE(sub.a_count == 1);
-    REQUIRE(sub.b_sum == 10);
-}
-
-TEST_CASE("Unsubscribing an object that was never subscribed is safe") {
-    Publisher pub;
-    TestSubscriber sub;
-
-    pub.unsubscribe<TestEvents::A>(&sub); // should be no-op, no crash
-    pub.emit<TestEvents::A>();
-    REQUIRE(sub.a_count == 0);
-}
-
-TEST_CASE("Emitting with no subscribers does nothing and is safe") {
-    Publisher pub;
-    REQUIRE_NOTHROW(pub.emit<TestEvents::A>());
-    REQUIRE_NOTHROW(pub.emit<TestEvents::B>(42));
-}
-
-TEST_CASE("Async event delivery still invokes subscribers") {
-    Publisher pub;
-    TestSubscriber sub;
-
-    sub.subscribe_to(pub);
-    pub.emit_async<TestEvents::B>(99);
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // allow async to run
-
-    REQUIRE(sub.b_sum == 99);
+    pub.emit<TestEvents::Ping>();
+    REQUIRE(total_calls == 2);
 }
